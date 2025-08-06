4929837cbbaa2d56ad5d84b4c1b1bf9e4df7f2c6
tientv34 <tvtien34@gmail.com>
2025-08-06 22:06:07 +0700

    update database

--- SachDB.sql
+++ SachDB.sql
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `accounting`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `accounting` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `user_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -54,9 +54,15 @@ CREATE TABLE `books` (
   `tacGia` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `nhaXuatBan` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `namXuatBan` int DEFAULT NULL,
-  `isbn` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `isbn` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `soLuong` int DEFAULT NULL,
   `theLoai_id` int DEFAULT NULL,
+  `nam_xuat_ban` int DEFAULT NULL,
+  `nha_xuat_ban` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `so_luong` int DEFAULT NULL,
+  `tac_gia` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ten_sach` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `the_loai` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `isbn` (`isbn`),
   KEY `fk_books_theLoai` (`theLoai_id`),
@@ -85,11 +91,15 @@ CREATE TABLE `chi_tiet_phieu_muon` (
   `phieuMuon_id` int DEFAULT NULL,
   `book_id` int DEFAULT NULL,
   `quantity` int DEFAULT '1',
+  `phieu_muon_id` int DEFAULT NULL,
+  `status` text,
   PRIMARY KEY (`id`),
   KEY `phieuMuon_id` (`phieuMuon_id`),
   KEY `book_id` (`book_id`),
+  KEY `FKmoxy18qf6frphyl8a7krmlcb` (`phieu_muon_id`),
   CONSTRAINT `chi_tiet_phieu_muon_ibfk_1` FOREIGN KEY (`phieuMuon_id`) REFERENCES `phieumuon` (`id`),
-  CONSTRAINT `chi_tiet_phieu_muon_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`)
+  CONSTRAINT `chi_tiet_phieu_muon_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  CONSTRAINT `FKmoxy18qf6frphyl8a7krmlcb` FOREIGN KEY (`phieu_muon_id`) REFERENCES `phieu_muon` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -113,10 +123,12 @@ CREATE TABLE `members` (
   `id` int NOT NULL AUTO_INCREMENT,
   `fullName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `phone` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `address` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
   `theLoai` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `membership_date` date DEFAULT NULL,
+  `membership_date` datetime(6) DEFAULT NULL,
+  `full_name` varchar(255) DEFAULT NULL,
+  `the_loai` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -128,6 +140,36 @@ CREATE TABLE `members` (
 LOCK TABLES `members` WRITE;
 /*!40000 ALTER TABLE `members` DISABLE KEYS */;
 /*!40000 ALTER TABLE `members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `phieu_muon`
+--
+
+DROP TABLE IF EXISTS `phieu_muon`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `phieu_muon` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `actual_return` datetime(6) DEFAULT NULL,
+  `create_date` datetime(6) DEFAULT NULL,
+  `loaiphieu` varchar(255) DEFAULT NULL,
+  `return_date` datetime(6) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `member_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKtrt1qm4k700k4o0gchemqna5x` (`member_id`),
+  CONSTRAINT `FKtrt1qm4k700k4o0gchemqna5x` FOREIGN KEY (`member_id`) REFERENCES `members` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `phieu_muon`
+--
+
+LOCK TABLES `phieu_muon` WRITE;
+/*!40000 ALTER TABLE `phieu_muon` DISABLE KEYS */;
+/*!40000 ALTER TABLE `phieu_muon` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -144,6 +186,7 @@ CREATE TABLE `phieumuon` (
   `return_date` date DEFAULT NULL,
   `actual_return` date DEFAULT NULL,
   `status` int DEFAULT NULL,
+  `loaiphieu` text,
   PRIMARY KEY (`id`),
   KEY `member_id` (`member_id`),
   CONSTRAINT `phieumuon_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `members` (`id`)
@@ -157,6 +200,30 @@ CREATE TABLE `phieumuon` (
 LOCK TABLES `phieumuon` WRITE;
 /*!40000 ALTER TABLE `phieumuon` DISABLE KEYS */;
 /*!40000 ALTER TABLE `phieumuon` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `the_loai`
+--
+
+DROP TABLE IF EXISTS `the_loai`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `the_loai` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ten_the_loai` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKa9n1rkctgi5feurkb3qqjr7y5` (`ten_the_loai`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `the_loai`
+--
+
+LOCK TABLES `the_loai` WRITE;
+/*!40000 ALTER TABLE `the_loai` DISABLE KEYS */;
+/*!40000 ALTER TABLE `the_loai` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -192,4 +259,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-31 22:41:56
+-- Dump completed on 2025-08-06 22:04:52
